c2ce7b4232badf1f1ba0d9db1460c58e580e43cb
Nokuphila <nokuphila23@gmail.com>
2023-03-23 14:03:42 -0700

    Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	ptintf("infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*      putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0)
+}
