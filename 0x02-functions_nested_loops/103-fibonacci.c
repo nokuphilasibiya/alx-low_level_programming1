dcb2fc1536f1575310c6dbe5098b2625e2f7dc9a
Nokuphila <nokuphila23@gmail.com>
2023-03-22 19:03:16 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of Even Fibinacci numbers
+ * less than 4000000.
+ * Return: Nothing!
+ */
+
+int main(void)
+
+{
+	int i = 0;
+	long j = 1, k=2, sum = k;
+
+	while (k + j < 4000000)
+	{
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	++i; }
+	printf("%ld\n", summ)
+	return (0);
+}
