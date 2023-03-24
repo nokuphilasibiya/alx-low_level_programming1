a1124c33c72fc578b1ab0b227700cdce79e1f147
Nokuphila <nokuphila23@gmail.com>
2023-03-24 08:39:13 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,8 +4,6 @@
  * main - Test function for positive or negative
  * Return: 0
  */
-
-
 int main(void)
 
 {
@@ -14,6 +12,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
